9460e464970a6ccc6066a9292255cbce8162b399
eric-czech <eric.allen.czech@gmail.com>
2020-02-18 18:47:11 -0500

    Removing relative paths from notebook import biolerplate (in canine gwas at least)

--- notebooks/env.sh
+++ notebooks/env.sh
@@ -1,7 +1,11 @@
-TUTORIAL_DIR=${HOME}/repos/gwas-analysis/notebooks/tutorial
-TUTORIAL_1_QC_DIR=${HOME}/data/gwas/tutorial/1_QC_GWAS
-TUTORIAL_2_PS_DIR=${HOME}/data/gwas/tutorial/2_PS_GWAS
-TUTORIAL_3_AA_DIR=${HOME}/data/gwas/tutorial/3_AA_GWAS
-ORGANISM_RICE_3KG_DIR=${HOME}/data/gwas/rice-snpseek/3K_RG_29mio_biallelic_SNPs_Dataset
-ORGANISM_CANINE_TGT_DIR=${HOME}/data/gwas/canine-hayward-2016-266k4
-ORGANISM_CANINE_REF_DIR=${HOME}/data/gwas/canine-parker-2017
+# NB_DIR=${HOME}/repos/gwas-analysis/notebooks # This should be set globally
+TUTORIAL_DIR=${NB_DIR}/tutorial
+BENCHMARK_DIR=${NB_DIR}/benchmark
+BENCHMARK_METHOD_DIR=${NB_DIR}/benchmark/method
+
+TUTORIAL_1_QC_DIR=${DATA_DIR}/gwas/tutorial/1_QC_GWAS
+TUTORIAL_2_PS_DIR=${DATA_DIR}/gwas/tutorial/2_PS_GWAS
+TUTORIAL_3_AA_DIR=${DATA_DIR}/gwas/tutorial/3_AA_GWAS
+ORGANISM_RICE_3KG_DIR=${DATA_DIR}/gwas/rice-snpseek/3K_RG_29mio_biallelic_SNPs_Dataset
+ORGANISM_CANINE_TGT_DIR=${DATA_DIR}/gwas/canine-hayward-2016-266k4
+ORGANISM_CANINE_REF_DIR=${DATA_DIR}/gwas/canine-parker-2017
