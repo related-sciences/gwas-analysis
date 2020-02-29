06a45c510ceb97494d31ff3464a0e2659882432a
eric-czech <eric.allen.czech@gmail.com>
2020-02-29 09:36:17 -0500

    Experiments with orthogonal randomized projections on HapMap LD (little difference)

--- notebooks/benchmark/method/common.py
+++ notebooks/benchmark/method/common.py
@@ -14,19 +14,48 @@ DATASET_CONFIG = {
     DATASET_SIM: dict(path=None, reference_genome='GRCh38')
 }
 
-def filename(filename, **props):
-    return '-'.join([filename] + [f'{k}={v}' for k, v in props.items()])
+def filename(basename, ext=None, **props):
+    fname = '-'.join([basename] + [f'{k}={v}' for k, v in props.items()])
+    return fname + ('' if ext is None else '.' + ext.replace('.', ''))
 
-def module_path(module, fname, ext=None, **props):
-    # Example: $REPO/notebooks/benchmark/method/ld_prune/lsh/04-analysis.ipynb -> ld_prune/lsh
-    relpath = osp.dirname(osp.relpath(ganb.get_notebook_path(), ga.BENCHMARK_METHOD_DIR))
-    fname = filename(fname, **props) + ('' if ext is None else '.' + ext.replace('.', ''))
-    # Example: $DATA_DIR/gwas/benchmark/$module/ld_prune/lsh/$filename
-    return osp.join(ga.BENCHMARK_METHOD_DATA_DIR, module, relpath, fname)
+class Method(object):
+    
+    def __init__(self, name, relpath):
+        self.name = name
+        self.relpath = relpath
+        
+    def module_path(self, module, path):
+        return osp.join(ga.BENCHMARK_METHOD_DATA_DIR, module, self.relpath, path)
+    
+    def module_file(self, module, basename, ext=None, **props):
+        return self.module_path(module, filename(basename or '', ext=ext, **props))
+    
+    def dataset_path(self, basename, ext=None, **props):
+        return self.module_file('datasets', basename, ext=ext, **props)
+    
+    def analysis_path(self, basename, ext=None, **props):
+        return self.module_file('analysis', basename, ext=ext, **props)
+    
+    @property
+    def render_dir(self):
+        return self.module_path('renders', '')
+        
+ld_prune_lsh = Method('ld-prune-lsh', 'ld_prune/lsh')
 
-def dataset_path(ds_name, **props):
-    return module_path('datasets', ds_name, **props)
+# def module_path(module, fname=None, ext=None, maxdepth=2, **props):
+#     # Example: $REPO/notebooks/benchmark/method/ld_prune/lsh/04-analysis.ipynb -> ld_prune/lsh
+#     relpath = osp.dirname(osp.relpath(ganb.get_notebook_path(), ga.BENCHMARK_METHOD_DIR))
+#     if maxdepth is not None:
+#         relpath = os.sep.join(relpath.split(os.sep)[:maxdepth])
+#     fname = filename(fname or '', **props) + ('' if ext is None else '.' + ext.replace('.', ''))
+#     # Example: $DATA_DIR/gwas/benchmark/$module/ld_prune/lsh/$filename
+#     return osp.join(ga.BENCHMARK_METHOD_DATA_DIR, module, relpath, fname)
 
+# def dataset_path(ds_name, **kwargs):
+#     return module_path('datasets', ds_name, **kwargs)
+
+# def render_path(fname=None, **kwargs):
+#     return module_path('renders', fname, **kwargs)
 
 def hash_collision_probability(a, h, g):
     """ Compute probability of hash collision for signed random projects (aka SimHash)
--- src/python/gwas_analysis/dask/__init__.py
+++ src/python/gwas_analysis/dask/__init__.py
@@ -1,14 +1,17 @@
 from gwas_analysis.dask import codecs
+from numcodecs.registry import register_codec
 
 
-def get_dask_client(n_workers, processes=True, n_threads=1, max_mem_fraction=.9):
+def get_dask_client(n_workers, processes=True, n_threads=1, max_mem_fraction=.9, plugins=None):
     import psutil
-    from numcodecs.registry import register_codec
     from dask.distributed import Client
 
-    register_codec(codecs.PackGeneticBits)
     ml = psutil.virtual_memory().total * max(min(max_mem_fraction, 1), 0)
     ml = str(int(ml // 1e9) // n_workers)
     client = Client(processes=processes, threads_per_worker=n_threads, n_workers=n_workers, memory_limit=ml + 'GB')
-    client.register_worker_plugin(codecs.CodecPlugin())
+
+    if plugins is not None and 'pgb' in plugins:
+        register_codec(codecs.PackGeneticBits)
+        client.register_worker_plugin(codecs.CodecPlugin())
+
     return client
