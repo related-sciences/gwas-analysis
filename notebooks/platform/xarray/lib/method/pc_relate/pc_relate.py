b788ea448e7c689398a74fc58a62156f1669e894
Rafal Wojdyla <ravwojdyla@gmail.com>
2020-05-05 01:39:29 +0900

    Dive into correctness of PC-Relate in Dask

--- notebooks/platform/xarray/lib/method/pc_relate/pc_relate.py
+++ notebooks/platform/xarray/lib/method/pc_relate/pc_relate.py
@@ -14,15 +14,23 @@ def __center(g: dask.array, mu: dask.array) -> dask.array:
     return g / 2 - mu
 
 
-def pc_relate(pcs: Union[dask.array.Array, np.ndarray], g: Union[dask.array.Array, np.ndarray]) -> dask.array:
+def pc_relate(pcs: Union[dask.array.Array, np.ndarray],
+              g: Union[dask.array.Array, np.ndarray],
+              maf: float = 0.01) -> dask.array:
     """
     Compute PC-Relate https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4716688/
 
     :param pcs: PCs that best capture population structure within g,
                 it has a length D
     :param g: variants x samples matrix, shape (v x s)
+    :param maf: individual minor allele frequency filter. If an individual's estimated
+                individual-specific minor allele frequency at a SNP is less than this value,
+                that SNP will be excluded from the analysis for that individual.
+                The default value is 0.01. Must be between (0.0, 1.0).
     :returns: (s x s) pairwise recent kinship estimation matrix
     """
+    if maf <= 0.0 or maf >= 1.0:
+        raise ValueError("MAF must be between (0.0, 1.0)")
     # 𝔼[gs|V] = 1β0 + Vβ, where 1 is a length _s_ vector of 1s, and β = (β1,...,βD)^T
     # is a length D vector of regression coefficients for each of the PCs
     pcsi = da.concatenate([da.from_array(np.ones((1, pcs.shape[1]))), pcs], axis=0).rechunk()
@@ -31,7 +39,8 @@ def pc_relate(pcs: Union[dask.array.Array, np.ndarray], g: Union[dask.array.Arra
     half_beta = da.matmul(da.matmul(da.linalg.inv(2 * r), q.T), g.T)
     mu = da.matmul(pcsi.T, half_beta).T
     # phi, eq: 4
-    variance = mu.map_blocks(lambda i: np.clip(i, 0.0, 1.0, i)).map_blocks(lambda i: i * (1.0 - i))
+    # TODO (rav): double check the MAF calculation
+    variance = mu.map_blocks(lambda i: np.clip(i, 0.0 + maf, 1.0 - maf, i)).map_blocks(lambda i: i * (1.0 - i))
     stddev = da.sqrt(variance)
     centered_af = da.blockwise(__center, "ij", g, "ij", mu, "ij")
     return gramian(centered_af) / gramian(stddev)
--- notebooks/platform/xarray/lib/method/pc_relate/pc_relate_test.py
+++ notebooks/platform/xarray/lib/method/pc_relate/pc_relate_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from .pc_relate import pc_relate
 import numpy as np
 
@@ -8,3 +10,16 @@ def test_pc_relate_dummy():
     rnd_g = np.random.rand(1000, s)
     r = pc_relate(rnd_pcs, rnd_g).compute()
     assert np.all(r < 0.5), "Relatedness estimations should be below 0.5"
+
+
+def test_bad_maf():
+    pcs = np.random.rand(2, 10)
+    g = np.random.rand(10, 10)
+    with pytest.raises(ValueError):
+        pc_relate(pcs, g, 1.0)
+    with pytest.raises(ValueError):
+        pc_relate(pcs, g, 0.0)
+    with pytest.raises(ValueError):
+        pc_relate(pcs, g, -1.0)
+    with pytest.raises(ValueError):
+        pc_relate(pcs, g, 2.0)
