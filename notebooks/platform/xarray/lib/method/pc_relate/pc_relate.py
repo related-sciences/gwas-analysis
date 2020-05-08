2d17afcf973a08a9fe84fef69d7db0b7803bd3c5
Rafal Wojdyla <ravwojdyla@gmail.com>
2020-05-09 03:50:30 +0900

    Fix up the correctness issues

--- notebooks/platform/xarray/lib/method/pc_relate/pc_relate.py
+++ notebooks/platform/xarray/lib/method/pc_relate/pc_relate.py
@@ -3,20 +3,28 @@ import dask
 import dask.array as da
 from typing import Union
 
+T_ARRAY = Union[dask.array.Array, np.ndarray]
 
-def gramian(a: dask.array) -> dask.array:
+
+def gramian(a: T_ARRAY) -> T_ARRAY:
     """Returns gramian matrix of the given matrix"""
     return da.dot(a.T, a)
 
 
-def __center(g: dask.array, mu: dask.array) -> dask.array:
-    # TODO (rav): double check: 0.0 if not mu else g / 2 - mu
-    return g / 2 - mu
+def __clip_mu_maf(mu: T_ARRAY, maf: float) -> T_ARRAY:
+    # TODO (rav): why  mu[(mu <= maf) & (mu >= 1 - maf)] doesnt work, do I need to use
+    #             bitwise_and (isn't it the same?)
+    mu[mu >= 1.0 - maf] = 1.0
+    mu[mu <= maf] = 1.0
+    return mu
 
 
-def pc_relate(pcs: Union[dask.array.Array, np.ndarray],
-              g: Union[dask.array.Array, np.ndarray],
-              maf: float = 0.01) -> dask.array:
+# TODO (rav):
+#  * double check MAF range (0.0, 1.0) or [0.0, 0.5]
+#  * add support for sample.include
+def pc_relate(pcs: T_ARRAY,
+              g: T_ARRAY,
+              maf: float = 0.01) -> T_ARRAY:
     """
     Compute PC-Relate https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4716688/
 
@@ -26,21 +34,24 @@ def pc_relate(pcs: Union[dask.array.Array, np.ndarray],
     :param maf: individual minor allele frequency filter. If an individual's estimated
                 individual-specific minor allele frequency at a SNP is less than this value,
                 that SNP will be excluded from the analysis for that individual.
-                The default value is 0.01. Must be between (0.0, 1.0).
+                The default value is 0.01. Must be between (0.0, 0.1).
     :returns: (s x s) pairwise recent kinship estimation matrix
     """
-    if maf <= 0.0 or maf >= 1.0:
+    if maf < 0.0 or maf > 1.0:
         raise ValueError("MAF must be between (0.0, 1.0)")
     # 𝔼[gs|V] = 1β0 + Vβ, where 1 is a length _s_ vector of 1s, and β = (β1,...,βD)^T
     # is a length D vector of regression coefficients for each of the PCs
+    # TODO: rechunk needs to go
     pcsi = da.concatenate([da.from_array(np.ones((1, pcs.shape[1]))), pcs], axis=0).rechunk()
     q, r = da.linalg.qr(pcsi.T)
     # mu, eq: 3
     half_beta = da.matmul(da.matmul(da.linalg.inv(2 * r), q.T), g.T)
     mu = da.matmul(pcsi.T, half_beta).T
     # phi, eq: 4
-    # TODO (rav): double check the MAF calculation
-    variance = mu.map_blocks(lambda i: np.clip(i, 0.0 + maf, 1.0 - maf, i)).map_blocks(lambda i: i * (1.0 - i))
+    clipped_mu = mu.map_blocks(lambda i: __clip_mu_maf(i, maf))
+    variance = clipped_mu.map_blocks(lambda i: i * (1.0 - i))
     stddev = da.sqrt(variance)
-    centered_af = da.blockwise(__center, "ij", g, "ij", mu, "ij")
+    centered_af = g / 2 - clipped_mu
+    # correction for clipped mu
+    centered_af[clipped_mu == 1.0] = 0.0
     return gramian(centered_af) / gramian(stddev)
