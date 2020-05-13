194aa67b90957a3d0a21f6be159710e41a8d237b
Rafal Wojdyla <ravwojdyla@gmail.com>
2020-05-13 19:07:01 +0900

    Validate missing value handling

--- notebooks/platform/xarray/lib/method/pc_relate/pc_relate.py
+++ notebooks/platform/xarray/lib/method/pc_relate/pc_relate.py
@@ -11,21 +11,17 @@ def gramian(a: T_ARRAY) -> T_ARRAY:
     return a.T.dot(a)
 
 
-def __clip_mu_maf(mu: T_ARRAY, maf: float) -> T_ARRAY:
-    # TODO (rav): why  mu[(mu <= maf) & (mu >= 1 - maf)] doesnt work, do I need to use
-    #             bitwise_and (isn't it the same?)
-    mu[mu >= 1.0 - maf] = 1.0
-    mu[mu <= maf] = 1.0
-    return mu
+def impute_with_variant_mean(g: T_ARRAY) -> (T_ARRAY, T_ARRAY):
+    """
+    Imputes missing values (negative or np.nan) with variant mean.
 
-
-def impute_with_sample_mean(g: T_ARRAY) -> T_ARRAY:
-    missing_values_are_nan = da.where(g >= 0.0, g, np.nan)
-    is_nan = da.isnan(missing_values_are_nan)
-    # TODO: can't use fancy indexing here. measure the impact of masked mask
-    return np.where(is_nan,
-                    da.ma.masked_array(missing_values_are_nan, mask=is_nan).mean(axis=0),
-                    missing_values_are_nan)
+    :param g: variants x samples matrix, shape (v x s)
+    :returns: tuple of (missing_values_mask, imputed_g)
+    """
+    neg_values_are_nan = da.where(g >= 0.0, g, np.nan)
+    is_nan_mask = da.isnan(neg_values_are_nan)
+    variant_mean = da.ma.masked_array(g, mask=is_nan_mask).mean(axis=1)[:, np.newaxis]
+    return is_nan_mask, da.where(is_nan_mask, variant_mean, g)
 
 
 # TODO (rav):
@@ -48,6 +44,7 @@ def pc_relate(pcs: T_ARRAY,
     """
     if maf < 0.0 or maf > 1.0:
         raise ValueError("MAF must be between (0.0, 1.0)")
+    missing_g_mask, imputed_g = impute_with_variant_mean(g)
     # 𝔼[gs|V] = 1β0 + Vβ, where 1 is a length _s_ vector of 1s, and β = (β1,...,βD)^T
     # is a length D vector of regression coefficients for each of the PCs
     # TODO: rechunk needs to go
@@ -56,13 +53,14 @@ def pc_relate(pcs: T_ARRAY,
     #             implementation
     q, r = da.linalg.qr(pcsi.T)
     # mu, eq: 3
-    half_beta = da.linalg.inv(2 * r).dot(q.T).dot(g.T)
+    half_beta = da.linalg.inv(2 * r).dot(q.T).dot(imputed_g.T)
     mu = pcsi.T.dot(half_beta).T
     # phi, eq: 4
-    clipped_mu = mu.map_blocks(lambda i: __clip_mu_maf(i, maf))
-    variance = clipped_mu.map_blocks(lambda i: i * (1.0 - i))
+    mask = (mu <= maf) | (mu >= 1.0 - maf) | missing_g_mask
+    mu_mask = da.ma.masked_array(mu, mask=mask)
+    variance = mu_mask.map_blocks(lambda i: i * (1.0 - i))
+    variance = da.ma.filled(variance, fill_value=0.0)
     stddev = da.sqrt(variance)
-    centered_af = g / 2 - clipped_mu
-    # correction for clipped mu
-    centered_af[clipped_mu == 1.0] = 0.0
+    centered_af = g / 2 - mu_mask
+    centered_af = da.ma.filled(centered_af, fill_value=0.0)
     return gramian(centered_af) / gramian(stddev)
